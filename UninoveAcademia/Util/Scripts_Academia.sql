8754de9276a08079a978df5ee1544e10f8958bca
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2015-11-27 13:22:20 -0200

    Preparando classes de conexão

---
+++ UninoveAcademia/Util/Scripts_Academia.sql
@@ -0,0 +1,8 @@
+﻿-- Criando tabela de Login
+Create Table Login (
+	Codigo int Primary Key,
+	Usuario Varchar(8) Not Null,
+	Senha Varchar(8) Not Null,
+	Tipo_Usuario Varchar Not Null,
+	Data_Criacao DateTime Not Null
+)
